79830c8ce3dd35557f560ba98605dcad6d209172
Denys <kostenkoden89@gmail.com>
2024-01-28 16:04:03 +0200

    test4

--- src/app/components/MobileHeaderNav/MobileHeaderNav.tsx
+++ src/app/components/MobileHeaderNav/MobileHeaderNav.tsx
@@ -9,7 +9,6 @@ import MainNav from "./MainNav";
 
 function MobileHeaderNav({ navLabels }: MobileHeaderNavProps) {
   console.log("hi");
-
   return (
     <div className="block absolute top-10 pt-4 px-6 w-screen left-0 overflow-hidden bg-white">
       <SearchInput />
